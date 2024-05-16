57c7a86044799a048451499646dcdf5cb9da2eab
AlexEro298 <AlexEro298@yandex.ru>
2024-05-16 09:01:44 +0500

    edit bash_script and edit save_config.py

--- save_config.py
+++ save_config.py
@@ -30,35 +30,36 @@ if __name__ == "__main__":
     # current date and time
     now_date = datetime.datetime.now().strftime("%Y%m%d")
     now_time = datetime.datetime.now().strftime("%H:%M:%S").replace(':', '.')
-    file_log = open(f'/samba/hosts/log/save_config_{now_date}_{now_time}.txt')
+    file_log = open(f'/samba/log/save_config/save_config_{now_date}_{now_time}.txt', 'w')
 
     for host in hosts:
-            #saving path
-            path = f'/samba/hosts/{host}/'
+        #saving path
+        path = f'/samba/hosts/{host}/'
 
-            #rotation
-            file_rotation(3, path)
+        #rotation
+        file_rotation(3, path)
 
-            try:
-                # connect device
-                dev = Device(host=host, user=username, password=password, gather_facts=False)
-                dev.open()
-                file_log.write(f'Connect to {host}, OK!\n')
-            except ConnectError as err:
-                file_log.write(f'Can\'t connect to device. {err} \n')
-                continue
+        try:
+            # connect device
+            dev = Device(host=host, user=username, password=password, gather_facts=False)
+            dev.open()
+            file_log.write(f'Connect to {host}, OK!\n')
 
-            # current_config
-            current_config = dev.cli('show configuration | display omit')
-            file_current_config = open(f'{path}current_config_{now_date}_{now_time}_{host}.txt', 'w')
-            file_current_config.write(current_config)
-            file_current_config.close()
+        except ConnectError as err:
+            file_log.write(f'Can\'t connect to device. {err} \n')
+            continue
 
-            # current_config_set
-            current_config_set = dev.cli('show configuration | display set')
-            file_current_config_set = open(f'{path}current_config_set_{now_date}_{now_time}_{host}.txt', 'w')
-            file_current_config_set.write(current_config_set)
-            file_current_config_set.close()
+        # current_config
+        current_config = dev.cli('show configuration | display omit')
+        file_current_config = open(f'{path}current_config_{now_date}_{now_time}_{host}.txt', 'w')
+        file_current_config.write(current_config)
+        file_current_config.close()
 
-            # disconnect device
-            dev.close()
+        # current_config_set
+        current_config_set = dev.cli('show configuration | display set')
+        file_current_config_set = open(f'{path}current_config_set_{now_date}_{now_time}_{host}.txt', 'w')
+        file_current_config_set.write(current_config_set)
+        file_current_config_set.close()
+
+        # disconnect device
+        dev.close()
